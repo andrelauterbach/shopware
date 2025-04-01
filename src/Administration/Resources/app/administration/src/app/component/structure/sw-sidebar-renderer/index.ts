e7a6d0d48ec688c99f2a806546df3781c102b5fc
seggewiss <s.seggewiss@shopware.com>
2025-04-01 13:37:34 +0200

    feat: Add sidebar sdk handlers

---
+++ src/Administration/Resources/app/administration/src/app/component/structure/sw-sidebar-renderer/index.ts
@@ -0,0 +1,26 @@
+/**
+ * @sw-package framework
+ */
+
+import { computed } from 'vue';
+import template from './sw-sidebar-renderer.html.twig';
+import './sw-sidebar-renderer.scss';
+
+const { Component } = Shopware;
+
+/**
+ * @private
+ */
+Component.register('sw-sidebar-renderer', {
+    template,
+
+    setup() {
+        const activeSidebar = computed(() => {
+            return Shopware.Store.get('sidebar').getActiveSidebar;
+        });
+
+        return {
+            activeSidebar,
+        };
+    },
+});
